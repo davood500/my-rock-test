82489cc1871ba7df9def3fc047cad78ed1b4dad9
davood500 <37255133+davood500@users.noreply.github.com>
2018-04-07 02:36:23 +0430

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,35 +1,41 @@
-USE_CAMERA_STUB := true
-BOARD_USES_GENERIC_AUDIO := true
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
--include vendor/rockchip/rk3368/BoardConfigVendor.mk
+# For rockchip rk3036 rk312x rk3288 rk3368 rk3366 rk3399 platforms
+ifneq ($(filter rk%, $(strip $(TARGET_BOARD_PLATFORM))), )
 
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := rk3368
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := rk29board
+PRODUCT_HAVE_IPP ?= true
+PRODUCT_HAVE_RKVPU ?= true
+PRODUCT_HAVE_NAND ?= true
+PRODUCT_HAVE_RKWIFI ?= true
+PRODUCT_HAVE_RFTESTTOOL ?= true
+PRODUCT_HAVE_GPS ?= true
+PRODUCT_HAVE_RKPHONE_FEATURES ?= true
+PRODUCT_HAVE_RKAPPS ?= true
+PRODUCT_HAVE_RKEBOOK ?= false
+PRODUCT_HAVE_DATACLONE ?= false
+PRODUCT_HAVE_ADBLOCK ?= true
+PRODUCT_HAVE_WEBKIT_DEBUG ?= false
+PRODUCT_HAVE_RKTOOLS ?= true
 
-TARGET_PROVIDES_INIT_RC := true
+else
 
-TARGET_NO_KERNEL := true
+# For sofia platforms
+PRODUCT_HAVE_RKVPU ?= true
+PRODUCT_HAVE_RKTOOLS ?= true
+PRODUCT_HAVE_PLUGINSVC ?= true
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00c00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x10000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x20000000
-BOARD_FLASH_BLOCK_SIZE := 16384
-
-WPA_SUPPLICANT_VERSION := VER_0_6_X
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_ARG      := ""
-WIFI_DRIVER_MODULE_NAME     := "wlan"
-
-BOARD_HAVE_BLUETOOTH := true
-
-BOARD_EGL_CFG := device/rockchip/rk3368/egl.cfg
-BOARD_NO_RGBX_8888 := true
+endif
